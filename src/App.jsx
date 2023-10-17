ea571e768b88eeeeb416d143ef93319205392d87
Shikhar0895 <shikhar0895@gmail.com>
2023-10-17 15:51:00 +0530

    added strict mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 import { Link, BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -38,4 +38,8 @@ const App = () => {
 // const container = document.getElementById("root");
 // const root = createRoot(container);
 // root.render(<App />);
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
